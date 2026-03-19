b01fcf42fc11e7efc8f7d81c5f594e9abe5bc5da
Alessandro Thea <alessandro.thea@cern.ch>
2026-03-19 09:39:49 +0100

    daqdataformats added

--- pydunedaqfmt/__init__.py
+++ pydunedaqfmt/__init__.py
@@ -1,5 +1,7 @@
 """pydunedaqfmt — Python bindings for DUNE DAQ detector data formats."""
+import daqdataformats    # noqa: F401
 import detdataformats    # noqa: F401
 import fddetdataformats  # noqa: F401
+from daqdataformats import *   # noqa: F403
 from detdataformats import *   # noqa: F403
 from fddetdataformats import *  # noqa: F403
